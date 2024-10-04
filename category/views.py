3712950c1fc7b223de87281e95de93ab75429a7d
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2024-10-04 11:14:45 +0600

    added the throttling and indexing also requirement file

--- category/views.py
+++ category/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Category
 from .serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = CategorySerializer  
 
 
