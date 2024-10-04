94c349abcc607db801fe3f59c51bb5974b96822d
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2024-10-04 11:27:57 +0600

    error solved for deploy

--- product_management/settings.py
+++ product_management/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-(5le!1havglj8hqf-6lyxe7!of98yhdgwx@p(cqp*g*m$i&3xa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
